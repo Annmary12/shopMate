18f892282d0a5dfbbccd9ab4967c35fef723cf5c
Annmary12 <annmaryamaka@yahoo.com>
2019-06-30 22:03:12 +0100

    configure test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />);
+  const routes = wrapper.find('Routes');
+
+  expect(routes.length).toBe(1);
 });
